0aa041c5550be08d6f4b37bc673bc060c2a08105
Sam Byrne <ssbyrne@uw.edu>
2023-12-09 09:35:46 -0800

    day 1 p 2 solved

--- 2023/day_01.R
+++ 2023/day_01.R
@@ -2,11 +2,10 @@
 # Common header
 .DAY <- "day_01"
 source("/mnt/share/code/ssbyrne/advent_of_code/2023/paths.R")
+sam <- readLines(PATHS$inputs)
 
 # Part 1 ----
 
-# read inputs
-sam <- readLines(PATHS$inputs)
 # all have at least one digit, great
 which(grepl("^[^0-9]*$", sam))
 
@@ -33,67 +32,38 @@ first_last_digit <- lapply(seq_along(sam), function(item){
 
 # Part 2 ----
 
-# Restart, assuming we need to grab numbers in text form -----
+# tricksy oneight run-on english digits
+test_string <- '3sevenoneight'
 
-# reference vectors
-num_eng <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-nums <- 1:9; names(nums) <- num_eng
-regex_num_eng <- paste(num_eng, collapse = "|")
-regex_nums <- paste(c("\\d", regex_num_eng), collapse = "|")
-# find all the digits in each string
-matches <- gregexpr(regex_nums, sam)
-# find position of first and last digit
-first_last_index <- lapply(matches, function(x){
-  a <- x[1]
-  b <- x[length(x)]
-  return(c(a,b))
-})
-# find length of first and last digit, whether int or char
-first_last_length <- lapply(seq_along(matches), function(item){
-  idx_a <- first_last_index[[item]][1]
-  idx_b <- first_last_index[[item]][2]
-  x <- attr(matches[[item]], "match.length")
-  a <- x[1]
-  b <- x[length(x)]
-  return(c(a,b))
-})
-# grab those digits as character
-first_last_as_char <- lapply(seq_along(matches), function(item){
-  pos_a <- first_last_index[[item]][1]
-  pos_b <- first_last_index[[item]][2]
-  len_a <- first_last_length[[item]][1]
-  len_b <- first_last_length[[item]][2]
+find_digits_int_eng <- function(x, nums){
   
-  idx_a <- pos_a:(pos_a + len_a - 1)
-  idx_b <- pos_b:(pos_b + len_b - 1)
-  
-  num_a <- substr(sam[[item]], idx_a[1], idx_a[length(idx_a)])
-  num_b <- substr(sam[[item]], idx_b[1], idx_b[length(idx_b)])
-  
-  return(c(num_a, num_b))
-})
-# covert character to integer
-first_last_digit <- lapply(first_last_as_char, function(x){
-  # x <- first_last_as_char[[2]]
-  ab <- list()
-  for(i in 1:2){
-    if(is.na(as.integer(x[i]))){
-      ab[i] <- nums[which(names(nums) == x[i])]
-    } else {
-      ab[i] <- as.integer(x[i])
+  matches <- vector(mode = 'character')
+  for(i_start in 1:nchar(x)){
+    # Extract integer digits
+    if(substr(x, i_start, i_start) %in% nums) matches <- c(matches, substr(x, i_start, i_start))
+    # Extract English digits
+    minL <- min(nchar(names(nums))) - 1
+    maxL <- max(nchar(names(nums))) - 1
+    for(i_end in minL:maxL){
+      bound <- i_start + i_end
+      if(substr(x, i_start, bound) %in% names(nums)) matches <- c(matches, substr(x, i_start, bound))
     }
   }
-  return(as.integer(paste(ab, collapse = "")))
-})
+  # convert from English to digit
+  digits <- vector('character')
+  for(m in matches){
+    digit <- match(m, names(nums))
+    if(is.na(as.integer(digit))) digit <- m
+    digits <- c(digits, digit)
+  }
+  return(as.integer(paste0(
+    c(digits[1], digits[length(digits)]),
+    collapse = ""
+  )))
+}
 
-(total_sum <- sum(unlist(first_last_digit)))
-
-# Troubleshoot
-.idx <- 555
-item <- .idx
-sam[[.idx]]
-matches[[.idx]]
-first_last_index[[.idx]]
-first_last_length[[.idx]]
-first_last_as_char[[.idx]]
-first_last_digit[[.idx]]
+sum(
+  unlist(
+    lapply(sam, find_digits_int_eng, nums)
+  )
+)
